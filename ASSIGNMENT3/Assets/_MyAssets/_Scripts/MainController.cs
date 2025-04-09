e86b34e4db93a63b5997180ac0a50f19ff7e231f
Orinn41 <boranakkoyun41@gmail.com>
2025-04-08 20:18:19 -0400

    made a sceneloader started to mainController

---
+++ ASSIGNMENT3/Assets/_MyAssets/_Scripts/MainController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    public static MainController instance {  get; private set; }
+    public SoundManager SoundManager { get; private set; }
+    public
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
